49ff6ec767ace0f0b8fcda39f9f8a47a604f6887
manh2309 <118231431+manh2309@users.noreply.github.com>
2022-11-19 08:56:25 +0700

    static File

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-// const express = require("express");
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 configViewEngine(app);
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 app.get("/home", (req, res) => {
-  res.send(`Hi I'm Manh`);
+  res.render("test.ejs");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
